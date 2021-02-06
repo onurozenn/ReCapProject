5bbb97796dc6b0891f7df944e5f55cbe43051abf
Onur Özen <onur_ozen@outlook.com>
2021-02-06 19:03:42 +0300

    03.02.2021

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -13,11 +14,11 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryCarDal()
         {
             _car = new List<Car> {
-            new Car{ CarId=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=300,Description="Büyük kırmızı araba" },
-            new Car{ CarId=2,BrandId=1,ColorId=2,ModelYear=2018,DailyPrice=200,Description="Büyük Mavi araba" },
-            new Car{ CarId=3,BrandId=2,ColorId=1,ModelYear=2015,DailyPrice=150,Description="Büyük Turuncu araba" },
-            new Car{ CarId=4,BrandId=2,ColorId=2,ModelYear=2019,DailyPrice=250,Description="Büyük Yeşil araba" },
-            new Car{ CarId=5,BrandId=2,ColorId=1,ModelYear=2017,DailyPrice=175,Description="Büyük Sarı araba" }
+            new Car{ CarId=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=300,Description=" Kırmızı araba" },
+            new Car{ CarId=2,BrandId=1,ColorId=2,ModelYear=2018,DailyPrice=200,Description=" Mavi araba" },
+            new Car{ CarId=3,BrandId=2,ColorId=1,ModelYear=2015,DailyPrice=150,Description=" Turuncu araba" },
+            new Car{ CarId=4,BrandId=2,ColorId=2,ModelYear=2019,DailyPrice=250,Description=" Yeşil araba" },
+            new Car{ CarId=5,BrandId=2,ColorId=1,ModelYear=2017,DailyPrice=175,Description=" Sarı araba" }
             };
         }
         public void Add(Car car)
@@ -33,9 +34,19 @@ namespace DataAccess.Concrete.InMemory
             _car.Remove(carToDelete);
         }
 
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _car;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetAllByBrand(int brandId)
